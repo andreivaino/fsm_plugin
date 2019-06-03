48948cdd93a1a44cb84c40cfa9167884396e0dff
Wade Wesolowsky <wadew@false.ca>
2019-06-02 22:59:35 -0600

    add comment

--- public/hack.js
+++ public/hack.js
@@ -4,4 +4,36 @@ $(document.body).on('keypress', function (event) {
   if (event.which === 58) {
     alert('boo!');
   }
+  else if (event.which === 65){
+  	console.log('trying query')
+  	$.get('127.0.0.1:9200/pfsense-*/_search', 
+  	{
+		"_source": ["@timestamp", "src_ip", "src_port", "proto", "dest_ip", "dest_port", "message"],
+		"query": {
+			"bool": {
+	 			"must": [
+	 				{"match": {"action" : "block"}}
+	 			],
+	 		"filter" : [
+	    		{"range": 
+		    		{
+		        		"@timestamp": {
+		             		"gte" : "now-1d/d",
+		              		"lt" :  "now/d"
+		            	}
+	        		}
+	    		}
+			]
+			}
+		}
+	},
+	function (data){
+		console.log(data);
+		alert('data was returned, check log')
+	},
+	'json'
+	);
+  }
 });
+
+
