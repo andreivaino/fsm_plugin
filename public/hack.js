08cc11a0b80f6784793d6fc6d72b6f046cd93fea
Wade Wesolowsky <wadew@false.ca>
2019-07-14 20:46:55 -0600

    Clean up hacks.

--- public/hack.js
+++ public/hack.js
@@ -1,38 +1,11 @@
+//Captures a keypress in the browser to display information.
+//Kept around for future development.
+
 import $ from 'jquery';
 
 $(document.body).on('keypress', function (event) {
   if (event.which === 58) {
-    alert('boo!');
-  }
-  else if (event.which === 65){
-  	console.log('trying query')
-  	$.get('127.0.0.1:9200/pfsense-*/_search', 
-  	{
-		"_source": ["@timestamp", "src_ip", "src_port", "proto", "dest_ip", "dest_port", "message"],
-		"query": {
-			"bool": {
-	 			"must": [
-	 				{"match": {"action" : "block"}}
-	 			],
-	 		"filter" : [
-	    		{"range": 
-		    		{
-		        		"@timestamp": {
-		             		"gte" : "now-1d/d",
-		              		"lt" :  "now/d"
-		            	}
-	        		}
-	    		}
-			]
-			}
-		}
-	},
-	function (data){
-		console.log(data);
-		alert('data was returned, check log')
-	},
-	'json'
-	);
+    alert('Event keypress caught.');
   }
 });
 
